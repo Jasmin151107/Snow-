ab03e17828787e0f24dc5685dafa489a977c1930
Jasmin151107 <79517547+Jasmin151107@users.noreply.github.com>
2021-05-16 15:07:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Body = Matter.Body
+const Bodies = Matter.Bodies
+
+var engine,world;
+ var girl, girlImg
+
+var bg 
+ var backgroundImg
+ var snow = []
+
+function preload(){
+getTime();
+
+girlImg = loadImage("girl.png");
+ 
+
+  bg1 = loadImage("snow2.jpg")
+}
+
+function setup() {
+  createCanvas(800,400);
+
+ girl = createSprite(400,200,50,50)
+girl.addImage("girl",girlImg);
+girl.scale = 1
+
+
+  engine = Engine.create();
+  world = engine.world;
+
+  
+}
+
+function draw() {
+  Engine.update(engine);
+  if(backgroundImg){
+  background(backgroundImg); 
+  }
+  else{
+    background(bg1);
+  }
+  
+girl.x = World.mouseX;
+ girl.y = World.mouseY;  
+
+ 
+
+  rand = Math.round(random(1,4));
+
+  if (frameCount% 5===0){
+    snow.push(new Snow(random(0,800),30,30))
+  }
+  for (var j = 0;j<snow.length; j++){
+    snow[j].display()
+  }
+  drawSprites();
+}
+
+async function getTime (){
+  var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata")
+  var responsejson = await response.json ();
+  var dateTime = responsejson.datetime
+  var hour = dateTime.slice (11,13);
+  if(hour>06&&hour<18)
+bg = "snow1.jpg"
+
+
+else {
+  bg = "snow2.jpg"
+
+}
+ backgroundImg = loadImage(bg)
+ console.log(dateTime);
+
+}
+
+
